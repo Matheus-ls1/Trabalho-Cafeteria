223c3fb9b539e9017eef41ebfebd6974cbd1f444
Armando Stein <armandostein@hotmail.com>
2024-05-28 22:16:06 -0300

    Update Cafeteria.py

--- Cafeteria.py
+++ Cafeteria.py
@@ -1,5 +1,5 @@
 print("Bem-Vindo a Cafeterias\n")
-
+print("teste")
 
 print("------- MENU PRINCIPAL --------\n1) Adicionar itens ao cardápio\n2) Excluir itens do cardápio\n3) Alterar itens do cardápio\n4) Buscar itens no cardápio\n5) Listar todos os itens do cardápio\n0) Encerrar Programa")
 
