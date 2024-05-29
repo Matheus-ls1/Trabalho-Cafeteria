3466914d428b2d05187d3d22439720a740d6c103
Matheus-ls1 <166852383+Matheus-ls1@users.noreply.github.com>
2024-05-28 22:02:18 -0300

    Add files via upload

---
+++ Cafeteria.py
@@ -0,0 +1,6 @@
+print("Bem-Vindo a Cafeteria\n")
+
+
+print("------- MENU PRINCIPAL --------\n1) Adicionar itens ao cardápio\n2) Excluir itens do cardápio\n3) Alterar itens do cardápio\n4) Buscar itens no cardápio\n5) Listar todos os itens do cardápio\n0) Encerrar Programa")
+
+acao = int(input("\nDigite o numero correspondente a ação:"))
