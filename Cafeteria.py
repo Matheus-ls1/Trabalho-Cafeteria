2b3590b57d4dd2aaebfc20047bee43bef99d36ff
Matheus-ls1 <166852383+Matheus-ls1@users.noreply.github.com>
2024-06-04 22:01:47 -0300

    Update Cafeteria.py
    estrutura principal

--- Cafeteria.py
+++ Cafeteria.py
@@ -1,6 +1,12 @@
+import csv
+
 print("Bem-Vindo a Cafeterias\n")
 
-print("------- MENU PRINCIPAL --------\n1) Adicionar itens ao cardápio\n2) Excluir itens do cardápio\n3) Alterar itens do cardápio\n4) Buscar itens no cardápio\n5) Listar todos os itens do cardápio\n0) Encerrar Programa")
-
 cardapio = []
-acao = int(input("\nDigite o numero correspondente a ação:"))
+
+while True:
+    print("\n------- MENU PRINCIPAL --------\n1) Adicionar itens ao cardápio\n2) Excluir itens do cardápio\n3) Alterar itens do cardápio\n4) Buscar itens no cardápio\n5) Listar todos os itens do cardápio\n0) Encerrar Programa")
+    acao = int(input("\nDigite o numero correspondente a ação:"))
+    if acao == 0:
+        break
+
